c978bcdcfae748730453567c6dcaff8d774210be
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-12-22 08:09:11 +0700

    feat: add route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ModemController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('modem/search', [ModemController::class, 'search'])->name('modem.search');
+Route::resource('/modem', ModemController::class);
